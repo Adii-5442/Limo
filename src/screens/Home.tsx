06d5533e551ccf6a092fd2f4a36cf7b49b2aef4c
Aditya Singh <adi.sh5442@gmail.com>
2024-01-22 00:57:00 +0530

    WIP - HomePage

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,35 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
 
 const Home = () => {
   const [text, setText] = useState('');
 
   return (
     <View style={styles.container}>
-      <View style={styles.textContainer}>
-        <Text>Your Limo at Service</Text>
+      <View style={{flexDirection: 'column', flex: 0.09}}>
+        <View style={styles.headerContainer}>
+          <Image
+            style={{height: 35, width: 35, margin: 10}}
+            source={require('../assets/menu.png')}
+          />
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: '700',
+              color: 'white',
+              marginTop: 15,
+            }}>
+            Limooo
+          </Text>
+          <Image
+            style={{height: 35, width: 35, margin: 10}}
+            source={require('../assets/threeDot.png')}
+          />
+        </View>
+        <View style={styles.line} />
       </View>
+
       <View style={styles.inputContainer}>
         <TextInput
           label="Type Here"
@@ -28,8 +48,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-between',
   },
-  textContainer: {
-    justifyContent: 'flex-start',
+  headerContainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+
+    alignItems: 'center', // Center vertically
+  },
+  line: {
+    backgroundColor: 'grey',
+    height: 2,
+    alignSelf: 'stretch', // Take full width
+      marginHorizontal: 10, // Adjust the margin as needed
+      marginVertical: 10,
+    borderRadius:50,
   },
   inputContainer: {
     justifyContent: 'flex-end',
