198a7e7dda4550d2316b5f212ddb17a0a03e3a43
Aditya Singh <adi.sh5442@gmail.com>
2024-01-22 15:15:42 +0530

    Voice integration

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,71 @@
-import React, {useState} from 'react';
-import {View,TextInput, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, TextInput, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Voice from '@react-native-voice/voice'
 
 const Home = () => {
   const [inputText, setInputText] = useState<string>('');
   const [micActive, setmicActive] = useState<true | false>(false)
-  const [showMic, setshowMic] = useState<true|false>(true)
+  const [showMic, setshowMic] = useState<true | false>(true)
+
+  const [isListening, setisListening] = useState(false)
+  const [messages, setmessages] = useState([])
+  const [RecognizedSpeech, setRecognizedSpeech] = useState('')
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = stopListening;
+    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechError = error => {
+      console.log('onSpeechError', error);
+      setisListening(false)
+
+    }
+
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners)
+      setisListening(false)
+    }
+  }, [])
+  const onSpeechStart = (event:any) => {
+    console.log("Recording Started ...",event)
+
+  }
+  const onSpeechResults = (event: any) => {
+    const text = event.value[0];
+    console.log("Results", text)
+
+    setRecognizedSpeech(text)
+    setInputText(text)
+    if (text.length > 0) {
+      setshowMic(false)
+    }
+
+
+  }
+
+  const startListening = async() => {
+    setisListening(true);
+    try {
+      await Voice.start('en-US');
+    } catch (error) {
+      console.log("Yo")
+    }
+  }
+
+  const stopListening = async () => {
+    setisListening(false);
+    try {
+      Voice.removeAllListeners();
+      await Voice.stop()
+
+    } catch (error) {
+      console.log("Got errors in StopListening", error)
+
+
+    }
+
+  }
 
   const handleTextChange = (text:string) => {
     if (text.length == 0) {
@@ -43,7 +104,7 @@ const Home = () => {
           </TouchableOpacity>
           <Text
             style={{
-              fontSize: 20,
+              fontSize: 25,
               fontWeight: '700',
               color: 'white',
               marginTop: '5%',
@@ -68,9 +129,11 @@ const Home = () => {
       <View style={styles.inputContainer}>
         <View style={{flexDirection: 'row'}}>
           <TextInput
-            placeholder='Type Here'
-            textBreakStrategy='balanced'
-            placeholderTextColor={'grey'}
+            placeholder={
+              isListening ? 'Listening .......' : 'Start Typing Here'
+            }
+            textBreakStrategy="balanced"
+            placeholderTextColor={isListening ? '#018280' : 'grey'}
             style={styles.textInput}
             value={inputText}
             textAlignVertical="top"
@@ -78,37 +141,42 @@ const Home = () => {
             // numberOfLines={4}
             multiline={true}
             underlineColorAndroid={'transparent'}
-            onChangeText={(text)=>handleTextChange(text)}/>
-          <TouchableOpacity
-            onPress={handleMicPress}
-            style={{
-              borderRadius: 20,
-              marginBottom: 10,
-              alignSelf: 'center',
-            }}>
-            <Image
-              style={{height: 35, width: 35}}
-              source={
-                showMic?
-                  micActive
-                  ? require('../assets/micActive2.png')
-                    : require('../assets/micInactive.png') :
-                  require('../assets/send.png')
-              }
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              borderRadius: 20,
-              marginBottom: 10,
-              marginLeft: 5,
-              alignSelf: 'center',
-            }}>
-            <Image
-              style={{height: 35, width: 35}}
-              source={require('../assets/gallery.png')}
-            />
-          </TouchableOpacity>
+            onChangeText={text => handleTextChange(text)}
+          />
+          <View style={{flexDirection:'row',alignContent:'space-between',justifyContent:'space-between'}}>
+            <TouchableOpacity
+              onPress={() => {
+                isListening ? stopListening() : startListening();
+              }}
+              style={{
+                borderRadius: 20,
+                marginBottom: 10,
+                alignSelf: 'center',
+              }}>
+              <Image
+                style={{height: 35, width: 35}}
+                source={
+                  showMic
+                    ? isListening
+                      ? require('../assets/micActive.png')
+                      : require('../assets/micInactive.png')
+                    : require('../assets/send.png')
+                }
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                borderRadius: 20,
+                marginBottom: 10,
+                marginLeft: '3%',
+                alignSelf: 'center',
+              }}>
+              <Image
+                style={{height: 35, width: 35}}
+                source={require('../assets/gallery.png')}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
@@ -131,9 +199,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'grey',
     height: 2,
     alignSelf: 'stretch', // Take full width
-      marginHorizontal: 10, // Adjuset the margin as needed
-      marginVertical: 10,
-    borderRadius:50,
+    marginHorizontal: 10, // Adjuset the margin as needed
+    marginVertical: 10,
+    borderRadius: 50,
   },
   inputContainer: {
     justifyContent: 'flex-end',
@@ -144,12 +212,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: 'black',
     borderWidth: 1.5,
-    borderColor:'cyan',
+    borderColor: '#018280',
     paddingHorizontal: 10,
     justifyContent: 'center',
     textAlignVertical: 'center',
     fontWeight: '500',
-    fontSize:15,
+    fontSize: 15,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     margin: 5,
